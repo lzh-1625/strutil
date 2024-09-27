a96ff4cc6e89530a18af16e6d479a174abae1023
liuzhihang1 <1625167628@qq.com>
2024-09-27 15:48:34 +0800

    update

--- strchain.go
+++ strchain.go
@@ -6,80 +6,80 @@ import (
 	"github.com/duke-git/lancet/strutil"
 )
 
-type StrUtil struct {
+type strchain struct {
 	string
 }
 
-func NewStrUtil(s string) *StrUtil {
-	su := new(StrUtil)
-	su.string = s
-	return su
+func NewStrChain(s string) *strchain {
+	sc := new(strchain)
+	sc.string = s
+	return sc
 }
 
-func (s *StrUtil) TrimSpace() *StrUtil {
+func (s *strchain) TrimSpace() *strchain {
 	s.string = strings.TrimSpace(s.string)
 	return s
 }
 
-func (s *StrUtil) ToUpper() *StrUtil {
+func (s *strchain) ToUpper() *strchain {
 	s.string = strings.ToUpper(s.string)
 	return s
 }
 
-func (s *StrUtil) ToLower() *StrUtil {
+func (s *strchain) ToLower() *strchain {
 	s.string = strings.ToLower(s.string)
 	return s
 }
 
-func (s *StrUtil) ReplaceAll(old, new string) *StrUtil {
+func (s *strchain) ReplaceAll(old, new string) *strchain {
 	s.string = strings.ReplaceAll(s.string, old, new)
 	return s
 }
 
 // 返回源字符串中特定字符串首次出现时的位置之后的子字符串。
-func (s *StrUtil) After(char string) *StrUtil {
+func (s *strchain) After(char string) *strchain {
 	s.string = strutil.After(s.string, char)
 	return s
 }
 
 // 返回源字符串中指定字符串最后一次出现时的位置之后的子字符串。
-func (s *StrUtil) AfterLast(char string) *StrUtil {
+func (s *strchain) AfterLast(char string) *strchain {
 	s.string = strutil.AfterLast(s.string, char)
 	return s
 }
 
 // 返回源字符串中指定字符串第一次出现时的位置之前的子字符串。
-func (s *StrUtil) Before(char string) *StrUtil {
+func (s *strchain) Before(char string) *strchain {
 	s.string = strutil.Before(s.string, char)
 	return s
 }
 
 // 返回源字符串中指定字符串最后一次出现时的位置之前的子字符串。
-func (s *StrUtil) BeforeLast(char string) *StrUtil {
+func (s *strchain) BeforeLast(char string) *strchain {
 	s.string = strutil.BeforeLast(s.string, char)
 	return s
 }
 
-func (s *StrUtil) Containers(char string) bool {
+func (s *strchain) Containers(char string) bool {
 	return strings.Contains(s.string, char)
 }
 
-func (s *StrUtil) IsBlank() bool {
+func (s *strchain) IsBlank() bool {
 	return strutil.IsBlank(s.string)
 }
 
-func (s *StrUtil) HasPrefix(prefix string) bool {
+func (s *strchain) HasPrefix(prefix string) bool {
 	return strings.HasPrefix(s.string, prefix)
 }
 
-func (s *StrUtil) HasSuffix(suffix string) bool {
+func (s *strchain) HasSuffix(suffix string) bool {
 	return strings.HasSuffix(s.string, suffix)
 }
 
-func (s *StrUtil) String() string {
+func (s *strchain) String() string {
 	return s.string
 }
 
-func (s *StrUtil) Bytes() []byte {
+func (s *strchain) Bytes() []byte {
 	return []byte(s.string)
 }
